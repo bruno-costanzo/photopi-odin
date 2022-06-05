ce3f6071ec945f359a9bd272fe4085ac880c2751
bruno-costanzo <dev.bcostanzo@gmail.com>
2022-06-05 11:08:40 -0300

    app working v1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,12 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def index
+    flickr = Flickr.new(Rails.application.credentials.flickr[:key], Rails.application.credentials.flickr[:secret])
+    if params[:flickr_id].present?  # If user_id is present, get the user's photos
+      @photos = flickr.photos.search(user_id: params[:flickr_id])
+    else # Otherwise, get the public photos
+      @photos = flickr.photos.getRecent
+    end
+  end
 end
